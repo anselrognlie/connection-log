6e0ebdb12f366504c588ec7d94645d79bb4f238e
Ansel Rognlie <ansel.rognlie@gmail.com>
2024-01-12 12:46:49 -0800

    add logging for headers

--- app/route/log.py
+++ app/route/log.py
@@ -1,10 +1,13 @@
 from flask import Blueprint, request, make_response
+from pprint import pformat
 
 bp = Blueprint("log", __name__)
 
 @bp.get("/")
 def index():
     print(f"request from: {request.remote_addr}")
+    headers = pformat(request.headers)
+    print(f"headers: {headers}")
 
     response = make_response("", 204)
     return response
